819275715ba6e58b3a170937fb2f4fe8fb8b13de
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2023-03-19 10:22:04 +0600

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Uchchhwas Roy on 19/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
